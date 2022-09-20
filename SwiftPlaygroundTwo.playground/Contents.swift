f8bfc8144258dca59653085b843a3f05075e34ed
lialivehi <111819496+lialivehi@users.noreply.github.com>
2022-09-19 14:57:07 -1000

    Completed parts 1-4

---
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I'm not doing very well"
+{
+    print("I should be getting an A at this point")
+}
+else
+{
+    print("I need to work harder in thix class")
+}
+var gradeA = true
+if gradeA == true
+{
+    print("I really like this class")
+}
+else
+{
+    print("I'm not so sure about coding in swift")
+}
